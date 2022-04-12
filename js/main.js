bcc244c9ddd39f900874a815695e7c118f69262b
timdoan22 <timdoan01@gmail.com>
2022-04-12 00:59:56 -0400

    embedding Google maps integration

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@ function getMap() {
     const country = document.querySelector('input').value
 
     fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(res => res.json()) //parse as JSON
+    .then(res => res.json())
     .then(data => {
-        console.log(data[0].flag)
         document.querySelector('h2').innerText = data[0].name.common
         document.querySelector('h2').innerText += ` ${data[0].flag}`
+        document.querySelector('iframe').classList.toggle('hidden')
+        document.querySelector('iframe').src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBfSsiOep5dUreACARWIRtM81bcnhFuMx4&q=${country}`
     })
     .catch(err => {
         console.log(`error ${err}`)
