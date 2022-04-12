8e0fc868e3c1ee88767fe5778e3c8a072f565574
timdoan22 <timdoan01@gmail.com>
2022-04-12 01:03:30 -0400

    refactored the DOM for the h2 innerText value

--- js/main.js
+++ js/main.js
@@ -6,8 +6,7 @@ function getMap() {
     fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(res => res.json())
     .then(data => {
-        document.querySelector('h2').innerText = data[0].name.common
-        document.querySelector('h2').innerText += ` ${data[0].flag}`
+        document.querySelector('h2').innerText = `${data[0].name.common} ${data[0].flag}`
         document.querySelector('iframe').classList.toggle('hidden')
         document.querySelector('iframe').src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBfSsiOep5dUreACARWIRtM81bcnhFuMx4&q=${country}`
     })
