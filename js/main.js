ba67bf2e26e1d3cc5abde130cf75efd828779b82
timdoan22 <timdoan01@gmail.com>
2022-04-12 00:20:28 -0400

    return country name and flag for country search call

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ function getMap() {
     fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(res => res.json()) //parse as JSON
     .then(data => {
-        console.log(data[0])
+        console.log(data[0].flag)
+        document.querySelector('h2').innerText = data[0].name.common
+        document.querySelector('h2').innerText += ` ${data[0].flag}`
     })
     .catch(err => {
         console.log(`error ${err}`)
