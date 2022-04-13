7486f44f1db29c926f6a1ae1f55114eea0ed1f29
timdoan22 <timdoan01@gmail.com>
2022-04-13 03:06:19 -0400

    getGeoData now fetches and returns the response for the temperature data

--- js/main.js
+++ js/main.js
@@ -5,13 +5,24 @@ document.getElementById('search').addEventListener('click', getGeoData)
 
 function getGeoData() {
     const country = document.querySelector('input').value
+    let latitude = 0
+    let longitude = 0
+
 
     fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(res => res.json())
     .then(data => {
-        const LATITUDE = data[0].latlng[0]
-        const LONGITUDE = data[0].latlng[1]
-
+        latitude = data[0].latlng[0]
+        longitude = data[0].latlng[1]
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${OPENWEATHER_API_KEY}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+        
         document.querySelector('h2').innerText = `${data[0].name.common} ${data[0].flag}`
         document.querySelector('iframe').classList.toggle('hidden')
         document.querySelector('iframe').src = `https://www.google.com/maps/embed/v1/place?key=${GOOGLE_API_KEY}&q=${country}`
