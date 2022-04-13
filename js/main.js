a377c2f91397b92649cfc8ae720867f50a1f3360
timdoan22 <timdoan01@gmail.com>
2022-04-13 02:39:34 -0400

    set up temperature html structure and changed getMap function to getGeoData function to fetch weather API data

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 const OPENWEATHER_API_KEY = config.OPENWEATHER_API_KEY
 const GOOGLE_API_KEY = config.GOOGLE_MAPS_KEY
 
-document.getElementById('search').addEventListener('click', getMap)
+document.getElementById('search').addEventListener('click', getGeoData)
 
-function getMap() {
+function getGeoData() {
     const country = document.querySelector('input').value
 
     fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(res => res.json())
     .then(data => {
+        const LATITUDE = data[0].latlng[0]
+        const LONGITUDE = data[0].latlng[1]
+
         document.querySelector('h2').innerText = `${data[0].name.common} ${data[0].flag}`
         document.querySelector('iframe').classList.toggle('hidden')
         document.querySelector('iframe').src = `https://www.google.com/maps/embed/v1/place?key=${GOOGLE_API_KEY}&q=${country}`
